41f7af8238a8f99a65f0e9b16cb4a55eab7848a0
diegoportela99 <diegofportela1@gmail.com>
2018-08-14 10:27:47 +1000

    Doctor_list xml and class added for a list view of buttons to click, to select the doctor from the databse

---
+++ app/src/main/java/me/regstudio/pd_app/Activities/DoctorList.java
@@ -0,0 +1,22 @@
+package me.regstudio.pd_app.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import me.regstudio.pd_app.R;
+
+/**
+ * Created by diego on 8/14/2018.
+ */
+
+public class DoctorList extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doctor_list);
+        setTitle("Doctor List");
+
+        
+    }
+}
--- app/src/main/java/me/regstudio/pd_app/Activities/SelectDoctor.java
+++ app/src/main/java/me/regstudio/pd_app/Activities/SelectDoctor.java
@@ -3,14 +3,21 @@ package me.regstudio.pd_app.Activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import butterknife.OnClick;
 import me.regstudio.pd_app.R;
 
 /**
  * Created by diego on 8/3/2018.
  */
 
+//Create a list of clickable buttons that link to a doctor
+
 public class SelectDoctor extends AppCompatActivity {
+
+    Button DocButton;
     MainActivity obj;
 
     @Override
@@ -19,7 +26,17 @@ public class SelectDoctor extends AppCompatActivity {
         setContentView(R.layout.select_doc);
 
 
+        DocButton = (Button) findViewById(R.id.ChangeDocB);
+
+
+        DocButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
 
     protected void selectDoc(String doc) {
         obj.selectDoctor(doc);
