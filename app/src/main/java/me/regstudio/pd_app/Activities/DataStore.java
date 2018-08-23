1dad85e0b306c6d9112fbfbc4a9025b31fac0d37
diegoportela99 <diegofportela1@gmail.com>
2018-08-23 20:02:36 +1000

    added DataStore class for method to store information

---
+++ app/src/main/java/me/regstudio/pd_app/Activities/DataStore.java
@@ -0,0 +1,34 @@
+package me.regstudio.pd_app.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by diego on 8/23/2018.
+ */
+
+//class used for storing information used by app and database
+public class DataStore {
+
+    //constructor
+    public DataStore() {
+
+    }
+
+    
+    //added to JSON files and or Folders to store information and upload to database.
+
+    public void addText() {}
+    public void addHeartRate() {}
+    public void addFile() {}
+    public void addVideo() {}
+    public void addPersonalInfo() {}
+
+
+    // functions may be used in next iteration
+
+    public String readText() {return "";}
+    public int readHeart() {return 0;}
+    public int readPersonalInfo() {return 0;}
+
+
+}
