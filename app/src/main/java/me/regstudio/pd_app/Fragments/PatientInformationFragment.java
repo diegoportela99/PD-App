aa8e5c951e0779c48408e31859229f5dd78d4471
nath1100 <nath.luu1@gmail.com>
2018-08-27 21:27:03 +1000

    Added EditText locking with ButterKnife and Edit/Save functionality

--- app/src/main/java/me/regstudio/pd_app/Fragments/PatientInformationFragment.java
+++ app/src/main/java/me/regstudio/pd_app/Fragments/PatientInformationFragment.java
@@ -3,14 +3,23 @@ package me.regstudio.pd_app.Fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import me.regstudio.pd_app.R;
 
 /**
@@ -31,10 +40,48 @@ public class PatientInformationFragment extends Fragment {
     //@BindView(R.id.frameLayout2)
     //TextView blankFragmentTV;
 
+    @BindView(R.id.edit_button) Button editButton;
+
+    @BindViews({
+            R.id.edit_first_name,
+            R.id.edit_last_name,
+            R.id.edit_sex,
+            R.id.edit_age,
+            R.id.edit_height,
+            R.id.edit_weight,
+            R.id.edit_condition
+    }) List<EditText> editTexts;
+
+    // Array used to store the InputTypes of the corresponding EditTexts.
+    static int[] editTextTypes = {
+            InputType.TYPE_CLASS_TEXT, // First Name
+            InputType.TYPE_CLASS_TEXT, // Last Name
+            InputType.TYPE_CLASS_TEXT, // Sex
+            InputType.TYPE_CLASS_NUMBER, // Age
+            InputType.TYPE_CLASS_NUMBER, // Height
+            InputType.TYPE_CLASS_NUMBER, // Weight
+            InputType.TYPE_TEXT_FLAG_IME_MULTI_LINE, // Condition
+    };
 
     public PatientInformationFragment() {
         // Required empty public constructor
     }
+
+    // ButterKnife Action that disables EditTexts' input.
+    static final ButterKnife.Action<EditText> DISABLE_EDIT = new ButterKnife.Action<EditText>() {
+        @Override
+        public void apply(@NonNull EditText view, int index) {
+            view.setInputType(InputType.TYPE_NULL);
+        }
+    };
+
+    // ButterKnife Action that enables EditTexts' input using preset input types.
+    static final ButterKnife.Action<EditText> ENABLE_EDIT = new ButterKnife.Action<EditText>() {
+        @Override
+        public void apply(@NonNull EditText view, int index) {
+            view.setInputType(editTextTypes[index]);
+        }
+    };
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -62,5 +109,24 @@ public class PatientInformationFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         // Now that the view has been created, we can use butter knife functionality
         //blankFragmentTV.setText("Welcome to this fragment");
+        // Disable all editTexts on initialisation.
+        ButterKnife.apply(editTexts, DISABLE_EDIT);
     }
+
+    @OnClick(R.id.edit_button)
+    public void editInfo(View view) {
+        /* Used to edit and save patient information */
+        // TODO: On startup, button requires two taps before this method seems to trigger. Needs to be fixed.
+        // Enable/Disable all EditTexts based on current state of the button.
+        if (editButton.getText() == "Edit") {
+            // Enable all EditTexts and change the button name
+            ButterKnife.apply(editTexts, ENABLE_EDIT);
+            editButton.setText("Save");
+        } else {
+            // Disable all EditTexts and change the button name
+            ButterKnife.apply(editTexts, DISABLE_EDIT);
+            editButton.setText("Edit");
+        }
+    }
+
 }
