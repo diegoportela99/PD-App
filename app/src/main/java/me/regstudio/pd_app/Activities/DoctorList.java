a335010bc39e73c9b1521c9b2f2eaee450fcfd02
Diego <diegofportela1@gmail.com>
2018-09-09 06:58:06 +1000

    added Spinner

--- app/src/main/java/me/regstudio/pd_app/Activities/DoctorList.java
+++ app/src/main/java/me/regstudio/pd_app/Activities/DoctorList.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -17,8 +18,7 @@ import me.regstudio.pd_app.R;
  */
 
 public class DoctorList extends AppCompatActivity {
-    private LinearLayout linearLayout;
-    private ScrollView scrollView;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
