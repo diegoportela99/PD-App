915bd5146f59eb17b63255a9ca3d322ceaa63e57
Diego <diegofportela1@gmail.com>
2018-09-15 12:41:16 +1000

    added multiple functions to app, large update

--- app/src/main/java/me/regstudio/pd_app/Activities/DoctorList.java
+++ app/src/main/java/me/regstudio/pd_app/Activities/DoctorList.java
@@ -6,18 +6,22 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import butterknife.OnClick;
 import me.regstudio.pd_app.R;
 
 /**
  * Created by diego on 8/14/2018.
  */
 
-public class DoctorList extends AppCompatActivity {
+public class DoctorList extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
     @Override
@@ -26,16 +30,27 @@ public class DoctorList extends AppCompatActivity {
         setContentView(R.layout.doctor_list);
         setTitle("Doctor List");
 
+        Spinner spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.doctors, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 
-
-    private void AddButtons() {
-
-    }
 
     private void setDoc(String x) {
         MainActivity.DoctorSelectedID = x;
     }
-
 }
