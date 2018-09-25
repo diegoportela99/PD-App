b7f971dd62e3c912b15b7d5dea7c2d0f79227957
nath1100 <nath.luu1@gmail.com>
2018-09-25 15:35:01 +1000

    Integrated Firebase storage and retrieval for patient information.

--- app/src/main/java/me/regstudio/pd_app/DataStructures/PatientFile.java
+++ app/src/main/java/me/regstudio/pd_app/DataStructures/PatientFile.java
@@ -1,7 +1,6 @@
 package me.regstudio.pd_app.DataStructures;
 
-import android.widget.EditText;
-
+import java.util.Arrays;
 import java.util.List;
 
 public class PatientFile {
@@ -10,21 +9,19 @@ public class PatientFile {
 
      */
 
-    public String first_name;
-    public String last_name;
-    public String sex;
-    public int age;
-    public double height;
-    public double weight;
-    public String condition;
+    private String user_id;
+
+    private String first_name;
+    private String last_name;
+    private String sex;
+    private int age;
+    private int height;
+    private int weight;
+    private String condition;
 
     public PatientFile() {}
 
-    public PatientFile(List<EditText> editTexts) {
-
-    }
-
-    public PatientFile(String first_name, String last_name, String sex, int age, double height, double weight, String condition) {
+    public PatientFile(String first_name, String last_name, String sex, int age, int height, int weight, String condition) {
         this.first_name = first_name;
         this.last_name = last_name;
         this.sex = sex;
@@ -33,5 +30,29 @@ public class PatientFile {
         this.weight = weight;
         this.condition = condition;
     }
+
+    public void addUserId(String user_id) { this.user_id = user_id;}
+    public String getUserId() { return this.user_id; }
+
+    public void addFirstName(String first_name) { this.first_name = first_name; }
+    public String getFirstName() { return this.first_name; }
+
+    public void addLastName(String last_name) { this.last_name = last_name; }
+    public String getLastName() { return this.last_name; }
+
+    public void addSex(String sex) { this.sex = sex; }
+    public String getSex() { return this.sex; }
+
+    public void addAge(int age) { this.age = age; }
+    public int getAge() { return this.age; }
+
+    public void addHeight(int height) { this.height = height; }
+    public int getHeight() { return this.height; }
+
+    public void addWeight(int weight) { this.weight = weight; }
+    public int getWeight() { return this.weight; }
+
+    public void addCondition(String condition) { this.condition = condition; }
+    public String getCondition() { return this.condition; }
 
 }
