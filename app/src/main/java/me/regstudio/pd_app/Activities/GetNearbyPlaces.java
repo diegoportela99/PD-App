e78fd6ce12f75a6efdb5dacd71faab8a08ff7fc2
tanmaymunigala <tanmay.munigala@gmail.com>
2018-10-02 00:35:03 +1000

    Added map functionality, with current location

---
+++ app/src/main/java/me/regstudio/pd_app/Activities/GetNearbyPlaces.java
@@ -0,0 +1,12 @@
+package me.regstudio.pd_app.Activities;
+
+import android.os.AsyncTask;
+
+
+public class GetNearbyPlaces extends AsyncTask<Object,String,String>
+{
+    @Override
+    protected String doInBackground(Object... objects) {
+        return null;
+    }
+}
